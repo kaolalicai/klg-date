12ee722faaf3198d5c577f29b64287744727dfa8
Nick <myfjdthink@163.com>
2018-08-28 15:29:28 +0800

    完善 dateUtil 的输入类型，不只是 Date

--- src/DateUtil.ts
+++ src/DateUtil.ts
@@ -2,6 +2,7 @@
  *
  * Created by nick on 2017/3/22.
  */
+import {MomentInput} from 'moment'
 import * as moment from 'moment-timezone'
 import {DurationInputArg1, unitOfTime} from 'moment-timezone'
 
@@ -15,7 +16,7 @@ export class DateUtil {
    * @param days 天数加减
    * @returns {Date}
    */
-  static getDayStart (date = new Date(), days = 0) {
+  static getDayStart (date: MomentInput = new Date(), days = 0) {
     return moment(date || new Date()).startOf('day').add(days, 'day').toDate()
   }
 
@@ -26,8 +27,9 @@ export class DateUtil {
    * @param type 类型 默认是天
    * @returns {Number}
    */
-  static diff (from, to = new Date(), type: unitOfTime.Diff = 'day') {
+  static diff (from: MomentInput, to?: MomentInput, type?: unitOfTime.Diff) {
     if (!from) return 0
+    if (!type) type = 'day'
     if (type === 'day') {
       from = moment(from).startOf('day').toDate()
       to = moment(to).startOf('day').toDate()
@@ -35,23 +37,23 @@ export class DateUtil {
     return moment(to).diff(from, type)
   }
 
-  static addDay (date = new Date(), days = 0) {
+  static addDay (date: MomentInput, days = 0) {
     return moment(date || new Date()).add(days, 'day').toDate()
   }
 
-  static addMonth (date = new Date(), months = 0) {
+  static addMonth (date: MomentInput, months = 0) {
     return this.add(date, months, 'month')
   }
 
-  static add (date = new Date(), amount: DurationInputArg1 = 0, unit: unitOfTime.DurationConstructor = 'day') {
+  static add (date: MomentInput, amount: DurationInputArg1 = 0, unit: unitOfTime.DurationConstructor = 'day') {
     return moment(date || new Date()).add(amount, unit).toDate()
   }
 
-  static format (date = null, format = 'YYYY-MM-DD') {
+  static format (date: MomentInput, format = 'YYYY-MM-DD') {
     return moment(date || new Date()).format(format)
   }
 
-  static compare (date1, date2 = new Date()) {
+  static compare (date1: MomentInput, date2: MomentInput = new Date()) {
     return moment(date1).diff(date2, 'seconds')
   }
 }
